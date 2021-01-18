1f9df33839c5c44dcec177deaf189459ea58415f
nlopez <nelly.lopez@utexas.edu>
2021-01-17 22:34:22 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,32 @@
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 const mysql = require("mysql");
 
 require('dotenv').config();
 
 //create connection to db
-// let sequelize;
+let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// }
-
-let connection
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    user: 'root',
-    password: 'mysql',
-    database: 'tech_blog'
+    dialect: 'mysql',
+    port: 3306
   });
-};
-connection.connect();
+}
 
-module.exports = connection;
+// let connection
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'mysql',
+//     database: 'tech_blog'
+//   });
+// };
+// connection.connect();
+
+module.exports = sequelize;
