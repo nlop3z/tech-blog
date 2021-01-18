9a927e457454d33eb087a963ae67c71ee06d29ca
nlopez <nelly.lopez@utexas.edu>
2021-01-17 22:05:53 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,26 +5,26 @@ require('dotenv').config();
 //create connection to db
 let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// }
-
-
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    user: 'root',
-    password: 'mysql',
-    database: 'tech_blog'
+    dialect: 'mysql',
+    port: 3306
   });
-};
+}
+
+
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'mysql',
+//     database: 'tech_blog'
+//   });
+// };
 
 module.exports = sequelize;
